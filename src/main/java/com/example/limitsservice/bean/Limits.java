2f493005d87c87b29d7d0d01b4970bd5ae169876
prudhvistunner <bhojanapallip1@udayton.edu>
2023-04-15 00:40:25 -0400

    First Commit

---
+++ src/main/java/com/example/limitsservice/bean/Limits.java
@@ -0,0 +1,29 @@
+package com.example.limitsservice.bean;
+
+public class Limits {
+	private int maximum;
+	private int minimum;
+
+	public Limits() {
+		super();
+	}
+	public Limits(int maximum, int minimum) {
+		super();
+		this.maximum = maximum;
+		this.minimum = minimum;
+	}
+	
+	public int getMaximum() {
+		return maximum;
+	}
+	public void setMaximum(int maximum) {
+		this.maximum = maximum;
+	}
+	public int getMinimum() {
+		return minimum;
+	}
+	public void setMinimum(int minimum) {
+		this.minimum = minimum;
+	}
+	
+}
